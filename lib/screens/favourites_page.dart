b9841dc0ec234f7865356f6783eb0ab2d3b411f1
kriticalflare <42350771+kriticalflare@users.noreply.github.com>
2019-12-31 23:37:48 +0530

    Add slide to delete to fav page

--- lib/screens/favourites_page.dart
+++ lib/screens/favourites_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:sies_library/components/BookListItem.dart';
 import 'package:sies_library/database/favourites_dao.dart';
 import 'package:sies_library/database/favourites_db.dart';
+import 'package:sies_library/models/book.dart';
 
 class FavouritesPage extends StatefulWidget {
   FavouritesPage({Key key}) : super(key: key);
@@ -13,6 +14,7 @@ class FavouritesPage extends StatefulWidget {
 class _FavouritesPageState extends State<FavouritesPage> {
   @override
   Widget build(BuildContext context) {
+    FavouritesDao favouritesDao = Provider.of<FavouritesDao>(context);
     return StreamBuilder<List<Favourite>>(
       stream: Provider.of<FavouritesDao>(context).watchAllFav(),
       builder: (BuildContext context, AsyncSnapshot<List<Favourite>> snapshot) {
@@ -26,10 +28,18 @@ class _FavouritesPageState extends State<FavouritesPage> {
           return ListView.builder(
             itemCount: snapshot.data.length,
             itemBuilder: (BuildContext context, int index) {
-              return BookListItem(
-                favourite: snapshot.data[index],
-                index: index,
-                pop: false,
+              Favourite item = snapshot.data[index];
+              return Dismissible(
+                key: Key(item.toString()),
+                background: Container(color: Colors.redAccent),
+                onDismissed: (direction){
+                  favouritesDao.deleteFav(Results.fromJsonDB(item.toJson()));
+                },
+                child: BookListItem(
+                  favourite: item,
+                  index: index,
+                  pop: false,
+                ),
               );
             },
           );
